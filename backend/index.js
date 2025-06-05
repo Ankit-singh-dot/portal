f35a83aaad07ce440ffc6e857d841346c1006378
Ankit <ankitrajdihara123@gmail.com>
2025-06-05 10:19:44 +0530

    m---3

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 import connectDB from "./config/database.js";
+import userRoute from "./routes/user.routes.js";
+import companyRoute from "./routes/company.routes.js";
+import jobRoutes from "./routes/job.routes.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api/users", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/jobs", jobRoutes);
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "welcome to the API",
